860b85b43bc67b68347f262c2b5f96c4e1832266
dodo19025 <ydodohdisme@gmail.com>
2026-06-29 22:26:14 +0100

    strike localisation

---
+++ TheMiddleNurseFatherOutisCode/Cards/middlestrike.cs
@@ -0,0 +1,29 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
+
+
+public class middlestrike()
+    : TheMiddleNurseFatherOutisCard(1,
+        CardType.Attack, CardRarity.Basic,
+        TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6m, ValueProp.Move)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        CommonActions.CardAttack(this, play, 1, vfx:"vfx/vfx_attack_slash", )
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
