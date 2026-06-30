91dd2f11d7a0a22ee828e994479dad4af6fa36d8
dodo19025 <ydodohdisme@gmail.com>
2026-06-30 11:29:52 +0100

    unpacking hover tips

--- TheMiddleNurseFatherOutisCode/Cards/Unpacking.cs
+++ TheMiddleNurseFatherOutisCode/Cards/Unpacking.cs
@@ -2,8 +2,10 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Potions;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
 using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
@@ -21,6 +23,12 @@ public class Unpacking() : TheMiddleNurseFatherOutisCard(
         new DamageVar(4m, ValueProp.Move),
         new DynamicVar("Power", 1m)
     });
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+    [
+        HoverTipFactory.FromPower<RisingFeverPower>(),
+        HoverTipFactory.FromPower<VulnerablePower>()
+    ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
