16a537e51c16de61d218b0c3351cfb2f649cf727
dodo19025 <ydodohdisme@gmail.com>
2026-06-30 13:49:39 +0100

    if statements oblitirates the game

--- TheMiddleNurseFatherOutisCode/Powers/RisingFeverPower.cs
+++ TheMiddleNurseFatherOutisCode/Powers/RisingFeverPower.cs
@@ -47,15 +47,6 @@ public class RisingFeverPower()
            Stage += 1;
         }
 
-        if (Stage == 1)
-        {
-            await PowerCmd.Apply<Powers.FirstSealRemovedPower>(choiceContext, base.Owner, 1m,base.Owner, null );
-        }
-        else if (Stage == 2)
-        {
-            await PowerCmd.Apply<Powers.SecondSealRemovedPower>(choiceContext, base.Owner, 1m,base.Owner, null );
-        }
-
                 
             
                
