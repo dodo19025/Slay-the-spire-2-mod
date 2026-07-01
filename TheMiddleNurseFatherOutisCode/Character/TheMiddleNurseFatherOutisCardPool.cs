25929ec6ae3700e4103b3144650a29f50ff5fbae
dodo19025 <ydodohdisme@gmail.com>
2026-07-01 18:18:15 +0100

    uhh

--- TheMiddleNurseFatherOutisCode/Character/TheMiddleNurseFatherOutisCardPool.cs
+++ TheMiddleNurseFatherOutisCode/Character/TheMiddleNurseFatherOutisCardPool.cs
@@ -16,9 +16,9 @@ public class TheMiddleNurseFatherOutisCardPool : CustomCardPoolModel
     They are applied as a shader onto an already colored image,
     so it may take some experimentation to find a color you like.
     Generally they should be values between 0 and 1. */
-    public override float H => 0.811f; //Hue; changes the color.
-    public override float S => 0.632f; //Saturation
-    public override float V => 0.761f; //Brightness
+    public override float H => 0.67f; //Hue; changes the color.
+    public override float S => 0.067f; //Saturation
+    public override float V => 0.791f; //Brightness
 
     //Alternatively, leave these values at 1 and provide a custom frame image.
     /*public override Texture2D CustomFrame(CustomCardModel card)
