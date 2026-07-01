8069ba161f461ae9f272295ae4d53f3d7c825bc7
dodo19025 <ydodohdisme@gmail.com>
2026-07-01 18:08:05 +0100

    few color changes

--- TheMiddleNurseFatherOutisCode/Character/TheMiddleNurseFatherOutisCardPool.cs
+++ TheMiddleNurseFatherOutisCode/Character/TheMiddleNurseFatherOutisCardPool.cs
@@ -16,7 +16,7 @@ public class TheMiddleNurseFatherOutisCardPool : CustomCardPoolModel
     They are applied as a shader onto an already colored image,
     so it may take some experimentation to find a color you like.
     Generally they should be values between 0 and 1. */
-    public override float H => 0.311f; //Hue; changes the color.
+    public override float H => 0.511f; //Hue; changes the color.
     public override float S => 0.732f; //Saturation
     public override float V => 0.761f; //Brightness
 
