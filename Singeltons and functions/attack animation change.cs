3ceeefe4b36ae78559cf69e4a459bd30aa7ef5b8
dodo19025 <ydodohdisme@gmail.com>
2026-07-02 18:18:23 +0100

    final sword and leg animation tests

--- Singeltons and functions/attack animation change.cs
+++ Singeltons and functions/attack animation change.cs
@@ -14,11 +14,11 @@ public class attack_animation_change(): CustomSingletonModel(HookType.Combat)
         {
             if (cardPlay.Card.Keywords.Contains(FervourKeyWord.Fervour))
             {
-                await CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "cast", 0.05f);
+                await CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "swordattack", 0.05f);
             }
             else if(!cardPlay.Card.Keywords.Contains(FervourKeyWord.Fervour))
             {
-                await CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "cast", 0.05f);
+                await CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "legattack", 0.05f);
             }
         }
     }
