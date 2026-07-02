92417d2c8a65c6c3affc31fbe80ff19e92144c36
dodo19025 <ydodohdisme@gmail.com>
2026-07-02 17:55:27 +0100

    changes for fervour attack type check and aniamtion

---
+++ Singeltons and functions/attack animation change.cs
@@ -0,0 +1,25 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheMiddleNurseFatherOutis;
+
+public class attack_animation_change(): CustomSingletonModel(HookType.Combat)
+{
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Type == CardType.Attack)
+        {
+            if (cardPlay.Card.Keywords.Contains(FervourKeyWord.Fervour))
+            {
+                CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "swordattack", 0.05f);
+            }
+            if (cardPlay.Card.Keywords.Contains(FervourKeyWord.Fervour) == false)
+            {
+                CreatureCmd.TriggerAnim(cardPlay.Card.Owner.Creature, "legattack", 0.05f);
+            }
+        }
+    }
+}
