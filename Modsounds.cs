a02678d4ccb052a2f7eed27ce6905ace7ced079f
dodo19025 <ydodohdisme@gmail.com>
2026-07-02 18:57:57 +0100

    biig oops

--- Modsounds.cs
+++ Modsounds.cs
@@ -4,8 +4,6 @@ namespace TheMiddleNurseFatherOutis;
 
 public static class Modsounds
 {
-   public static readonly ModSound Stompvfx = new("res://TheMiddleNurseFatherOutisCode/vfx/stompvfx.ogg");
-   public static readonly ModSound Swordvfx = new("res://TheMiddleNurseFatherOutisCode/vfx/swordvfx.ogg");
-   public static readonly ModSound unpacking0 = new("res://TheMiddleNurseFatherOutisCode/vfx/unpacking0.ogg");
+   public static readonly ModSound unpacking0 = new("res:///vfx/unpacking.ogg");
 }
 
