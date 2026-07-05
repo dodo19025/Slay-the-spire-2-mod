77d9d36c4e31cf921f9e5db6b27d984ff0f99a54
dodo19025 <ydodohdisme@gmail.com>
2026-07-05 01:34:43 +0100

    burn

---
+++ TheMiddleNurseFatherOutisCode/Powers/BurnPower.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using BaseLib.Hooks;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+
+
+public class BurnPower() : TheMiddleNurseFatherOutisPower
+{
+    public override PowerType Type =>
+        PowerType.Debuff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (!participants.Contains(base.Owner))
+        {
+            return;
+        }
+        await CreatureCmd.Damage(new ThrowingPlayerChoiceContext(), base.Owner,base.Amount,ValueProp.Unpowered,null);
+        if (base.Owner.IsAlive)
+        {
+            await PowerCmd.Decrement(this);
+        }
+        else
+        {
+            await Cmd.CustomScaledWait(0.1f, 0.25f);
+        }
+    }
+}
