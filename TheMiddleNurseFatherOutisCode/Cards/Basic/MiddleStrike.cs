f033ad567fdcedbbb58f0bb25b0845e14fdf241d
dodo19025 <ydodohdisme@gmail.com>
2026-07-07 08:21:02 +0100

    blleed loclisation and whatn ot

--- TheMiddleNurseFatherOutisCode/Cards/Basic/MiddleStrike.cs
+++ TheMiddleNurseFatherOutisCode/Cards/Basic/MiddleStrike.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
@@ -21,6 +22,11 @@ public class MiddleStrike()
     protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { CardTag.Strike };
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6m, ValueProp.Move)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<BleedPower>()
+    ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
