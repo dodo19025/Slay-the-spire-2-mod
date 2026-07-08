bb91864b76c005a703b60d351738396137e53d07
dodo19025 <ydodohdisme@gmail.com>
2026-07-08 15:58:35 +0100

    is it you?!

---
+++ TheMiddleNurseFatherOutisCode/Cards/Common/IsItYou.cs
@@ -0,0 +1,48 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Character;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers.Card_Powers;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards.Common;
+[Pool(typeof(TheMiddleNurseFatherOutisCardPool))]
+
+
+public class IsItYou() : TheMiddleNurseFatherOutisCard(
+    1, CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => (new DynamicVar[4]
+    {
+        new DamageVar(7m, ValueProp.Move),
+        new DynamicVar("Exclamation", 1m),
+        new DynamicVar("BleedNextAttack",1m),
+        new DynamicVar("BleedPowerAmount",1m)
+    });
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+    [
+        HoverTipFactory.FromPower<BleedPower>()
+    ];
+
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.CardAttack(this,play.Target).Execute(choiceContext);
+        await PowerCmd.Apply<BleedNextAttackPower>(choiceContext,base.Owner.Creature,base.DynamicVars["BleedNextAttack"].BaseValue,base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["BleedNextAttack"].UpgradeValueBy(1m);
+
+    }
+}
---
+++ TheMiddleNurseFatherOutisCode/Powers/Card Powers/BleedNextAttackPower.cs
@@ -0,0 +1,62 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards.Common;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers.Card_Powers;
+
+
+public class BleedNextAttackPower()
+    : TheMiddleNurseFatherOutisPower
+{
+    public override PowerType Type =>
+        PowerType.Debuff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    private const string _attackAppliedBleedKey = "AttackAppliedBleed";
+
+    private bool AppliedBleed = false;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => (new DynamicVar[1]
+    {
+        new DynamicVar("BleedAmount",1m)
+    });
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+    [
+        HoverTipFactory.FromPower<BleedPower>()
+    ];
+
+    
+    
+    public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer,
+        DamageResult result, ValueProp props,
+        Creature? target, CardModel? cardSource)
+    {
+        if (dealer == base.Owner && target != null && cardSource != null && props.IsPoweredAttack())
+        {
+            await PowerCmd.Apply<BleedPower>(choiceContext,target,base.DynamicVars["BleedAmount"].BaseValue, base.Owner,cardSource);
+            AppliedBleed = true;
+
+        }
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Type == CardType.Attack && cardPlay.Card.Owner.Creature == base.Owner &&
+            cardPlay.Card.CurrentTarget != null && base.Amount > 0 && AppliedBleed) //will still break somehow btw
+        {
+            await PowerCmd.Decrement(this);
+            AppliedBleed = false;
+        }
+    }
+}
