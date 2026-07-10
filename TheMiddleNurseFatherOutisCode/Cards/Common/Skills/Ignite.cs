10f57f0be65ec637f368a302b8beb26f0bcdde72
dodo19025 <ydodohdisme@gmail.com>
2026-07-10 22:27:06 +0100

    smaller numbers overall

--- TheMiddleNurseFatherOutisCode/Cards/Common/Skills/Ignite.cs
+++ TheMiddleNurseFatherOutisCode/Cards/Common/Skills/Ignite.cs
@@ -35,7 +35,7 @@ public class Ignite() : TheMiddleNurseFatherOutisCard(
     
     protected override IEnumerable<DynamicVar> CanonicalVars => 
     [
-        new DynamicVar("BurnApply", 12m),
+        new DynamicVar("BurnApply", 10m),
     ];
 
 
