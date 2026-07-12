b5b06842b3553dadfe059f3d7dae2e4563cb006a
dodo19025 <ydodohdisme@gmail.com>
2026-07-12 19:04:17 +0100

    write sins card test

---
+++ TheMiddleNurseFatherOutisCode/Cards/Common/Attacks/WriteSins.cs
@@ -0,0 +1,44 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards.Common;
+
+
+public class WriteSins() : TheMiddleNurseFatherOutisCard(
+    1, CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new DamageVar(9m,ValueProp.Move),
+        new CardsVar(1)
+    ];
+
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.CardAttack(this,play.Target).Execute(choiceContext);
+        CardModel cardModel =
+            (await CardSelectCmd.FromCombatPile(prefs: new CardSelectorPrefs(base.SelectionScreenPrompt, 1),
+                context: choiceContext, pile: PileType.Draw.GetPile(base.Owner), player: base.Owner)).FirstOrDefault(); //gives a selection prompt for the card correct card pile basically (and lets you select 1)
+        if (cardModel != null)
+        {
+            await CardPileCmd.Add(cardModel, PileType.Discard);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(3m);
+
+    }
+}
