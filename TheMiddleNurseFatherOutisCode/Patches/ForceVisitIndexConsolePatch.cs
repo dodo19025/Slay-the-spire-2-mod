2e9bdd68fc8bcbb2626e04b730d31061c0b07097
dodo19025 <ydodohdisme@gmail.com>
2026-07-12 04:34:18 +0100

    anctient cmd from a really kind person

---
+++ TheMiddleNurseFatherOutisCode/Patches/ForceVisitIndexConsolePatch.cs
@@ -0,0 +1,18 @@
+﻿using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Console;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Patches;
+
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Ancients;
+ 
+[HarmonyPatch(typeof(AncientDialogueSet), nameof(AncientDialogueSet.GetValidDialogues))]
+static class ForceVisitIndexConsolePatch
+{
+    static void Prefix(ref int charVisits, ref int totalVisits)
+    {
+        if (AncientDebug.ForcedVisitIndex is not { } v) return;
+        charVisits = v;
+        totalVisits = Math.Max(totalVisits, 1);
+        AncientDebug.ForcedVisitIndex = null;   
+    }
+}
