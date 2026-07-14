7883aef07c36a6aaba44843ac7ec70aedc92e4f9
dodo19025 <ydodohdisme@gmail.com>
2026-07-14 10:31:54 +0100

    tests for finding if you took damage last turn

--- TheMiddleNurseFatherOutisCode/Extensions/DamageTakenHook.cs
+++ TheMiddleNurseFatherOutisCode/Extensions/DamageTakenHook.cs
@@ -1,5 +1,6 @@
 ﻿using BaseLib.Abstracts;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -14,5 +15,34 @@ public class DamageTakenHook() : CustomSingletonModel(HookType.Combat)
     public static readonly SpireField<PlayerCombatState, bool> TookDamageLastTurn =
         new SpireField<PlayerCombatState, bool>(() => false);
 
+    public override Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props,
+        Creature? dealer, CardModel? cardSource)
+    {
+        if (dealer == null ||  dealer.Side == target.Side || !props.IsPoweredAttack() || dealer.IsPlayer || !target.IsPlayer)
+        {
+            return Task.CompletedTask;
+        }
 
+        if (target.IsPlayer && result.UnblockedDamage > 0)
+        {
+            TookDamageLastTurn[target.Player.PlayerCombatState] = true;
+            MainFile.Logger.Info($"Damage Last turn Status: {DamageTakenHook.TookDamageLastTurn[target.Player.PlayerCombatState]}");
+
+        }
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        foreach (var creature in participants)
+        {
+            if (creature.IsPlayer)
+            {
+                TookDamageLastTurn[creature.Player.PlayerCombatState] = false;
+                MainFile.Logger.Info($"Damage Last turn Status: {DamageTakenHook.TookDamageLastTurn[creature.Player.PlayerCombatState]}");
+            }
+        }
+
+        return Task.CompletedTask;
+    }
 }
