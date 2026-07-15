2ddf58fe40dc57523535ca877d7f2e8caec37188
dodo19025 <ydodohdisme@gmail.com>
2026-07-15 17:22:11 +0100

    paralyze working on

--- TheMiddleNurseFatherOutisCode/Cards/Basic/MiddleStrike.cs
+++ TheMiddleNurseFatherOutisCode/Cards/Basic/MiddleStrike.cs
@@ -31,6 +31,8 @@ public class MiddleStrike()
         CardPlay play)
     {
         await CommonActions.CardAttack(this, play.Target).Execute(choiceContext);
+        await PowerCmd.Apply<ParalyzePower>(choiceContext, play.Target, base.DynamicVars["Power"].BaseValue,
+            base.Owner.Creature, this);
     }
 
 
---
+++ TheMiddleNurseFatherOutisCode/Powers/Generic Powers/ParalyzePower.cs
@@ -0,0 +1,65 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models.Relics;
+using MegaCrit.Sts2.Core.MonsterMoves.Intents;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Powers;
+
+
+public class ParalyzePower() : TheMiddleNurseFatherOutisPower
+{
+    
+    public override PowerType Type =>
+        PowerType.Debuff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new DynamicVar("ReducedDamagePresen",50m),
+        new DynamicVar("ReducedDamage",0.5m),
+        new DynamicVar("MonsterDamage",0m),
+        new DynamicVar("MonsterHits",0m)
+    ];
+    
+    public override decimal ModifyDamageMultiplicative(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource, CardPlay? cardPlay)
+    {
+        if (dealer != base.Owner)
+        {
+            return 1m;
+        }
+        if (!props.IsPoweredAttack())
+        {
+            return 1m;
+        }
+        if (base.Owner.IsMonster)
+        {
+
+            int? attacks = Owner.Monster.NextMove.Intents.Sum(intent => intent is AttackIntent attackIntent ? attackIntent.Repeats : 0); //checks how many times this creature is attacking
+            int? damage = target?.Monster?.NextMove?.Intents.Where(static i => i is AttackIntent).Cast<AttackIntent>().FirstOrDefault()?.GetSingleDamage([target], base.Owner.Monster.Creature);
+            base.DynamicVars["MonsterHits"].BaseValue = (decimal)attacks;
+            base.DynamicVars["MonsterDamage"].BaseValue = (decimal)damage;
+        }
+        decimal num = base.DynamicVars["ReducedDamage"].BaseValue;
+        PowerCmd.Decrement(this);
+        return num;
+    }
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier,
+        CardModel? cardSource)
+    {
+        if (power == this)
+        {
+
+        }
+    }
+}
