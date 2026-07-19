962f5744c3240d36902d2c481ecaafd6d2612613
dodo19025 <ydodohdisme@gmail.com>
2026-07-19 18:37:59 +0100

    i just want to be freeeheee

--- TheMiddleNurseFatherOutisCode/Grudge/GrudgeResource.cs
+++ TheMiddleNurseFatherOutisCode/Grudge/GrudgeResource.cs
@@ -1,4 +1,4 @@
-﻿using BaseLib.Abstracts;
+using BaseLib.Abstracts;
 using BaseLib.Utils;
 using Godot;
 using MegaCrit.Sts2.Core.Entities.Players;
@@ -6,143 +6,84 @@ using MegaCrit.Sts2.Core.Nodes.Combat;
 
 namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Grudge;
 
-public static class GrudgeResource
+public static partial class GrudgeResource
 {
-    private static readonly SpireField<PlayerCombatState, int> PlayerGrudge = new(() => 0);
-    
-    public static event Action<PlayerCombatState, int, int>? GrudgeChanged;
+	private static readonly SpireField<PlayerCombatState, int> PlayerGrudge = new(() => 0);
+	
+	public static event Action<PlayerCombatState, int, int>? GrudgeChanged;
 
 
-    public static int GetGrudge(Player player) => 
-        player.PlayerCombatState != null ? PlayerGrudge[player.PlayerCombatState] : 0; //this should do what it says? aka ??
+	public static int GetGrudge(Player player) => 
+		player.PlayerCombatState != null ? PlayerGrudge[player.PlayerCombatState] : 0; //this should do what it says? aka ??
 
-    public static bool CanSpendGrudge(Player player)
-    {
-        if (player.PlayerCombatState == null) return false;
-        if (GetGrudge(player) <= 0) return false; //basically if the player does not have grudge to begin with they can't spend it
-        return true; //the player can spend Grudge if they have anything higher than 0
-    }
+	public static bool CanSpendGrudge(Player player)
+	{
+		if (player.PlayerCombatState == null) return false;
+		if (GetGrudge(player) <= 0) return false; //basically if the player does not have grudge to begin with they can't spend it
+		return true; //the player can spend Grudge if they have anything higher than 0
+	}
 
-    public static async Task GainGrudge(int amount, Player player)
-    {
-        if(player.PlayerCombatState == null || player.Creature.CombatState == null) return;
-        
-        //what is about to happen next is basically the process of how you gain stars one by one in the display
-        for (var i = 0; i < amount; i++)
-        {
-            MainFile.Logger.Info($"Added {i} Grudge");
-            var OldVar = PlayerGrudge[player.PlayerCombatState];
-            PlayerGrudge[player.PlayerCombatState] = OldVar + i;
-            GrudgeChanged?.Invoke(player.PlayerCombatState,OldVar,OldVar+1);
+	public static async Task GainGrudge(int amount, Player player)
+	{
+		if(player.PlayerCombatState == null || player.Creature.CombatState == null) return;
+		
+		//what is about to happen next is basically the process of how you gain stars one by one in the display
+		for (var i = 0; i < amount; i++)
+		{
+			MainFile.Logger.Info($"Added {i} Grudge");
+			var OldVar = PlayerGrudge[player.PlayerCombatState];
+			PlayerGrudge[player.PlayerCombatState] = OldVar + i;
+			GrudgeChanged?.Invoke(player.PlayerCombatState,OldVar,OldVar+1);
 
-            foreach (var model in player.Creature.CombatState.IterateHookListeners().ToList())
-            {
-                //then come back ONCE you make hooks for gainign grudge
-            }
-        }
-        MainFile.Logger.Info($"Added {amount} total Grudge");
+			foreach (var model in player.Creature.CombatState.IterateHookListeners().ToList())
+			{
+				//then come back ONCE you make hooks for gainign grudge
+			}
+		}
+		MainFile.Logger.Info($"Added {amount} total Grudge");
 
-        
-    }
+		
+	}
 
-    internal static void LoseGrudge(int amount, Player player)
-    {
-        if (player.PlayerCombatState == null) return;
-        var OldVar = PlayerGrudge[player.PlayerCombatState];
-        var newVar = Math.Max(0, OldVar - amount); //check if basically you reach 0... oh that is alot simpler than i thought
-        if (newVar == OldVar) return;
-        PlayerGrudge[player.PlayerCombatState] = newVar; //basically changes the number to the new one now after losing it
-        GrudgeChanged?.Invoke(player.PlayerCombatState,OldVar,newVar);
-    }
+	internal static void LoseGrudge(int amount, Player player)
+	{
+		if (player.PlayerCombatState == null) return;
+		var OldVar = PlayerGrudge[player.PlayerCombatState];
+		var newVar = Math.Max(0, OldVar - amount); //check if basically you reach 0... oh that is alot simpler than i thought
+		if (newVar == OldVar) return;
+		PlayerGrudge[player.PlayerCombatState] = newVar; //basically changes the number to the new one now after losing it
+		GrudgeChanged?.Invoke(player.PlayerCombatState,OldVar,newVar);
+	}
 
-    public class GrudgeCounterController() : CustomSingletonModel(HookType.Combat)
-    {
-        public static readonly AddedNode<NEnergyCounter, NGrudgeCounter> GrudgeCounterNode = new(parent =>
-        {
-            var counter = new NGrudgeCounter
-            {
-                Name = "GrudgeCounter",
-                MouseFilter = Control.MouseFilterEnum.Ignore
-            };
+	public class GrudgeCounterController() : CustomSingletonModel(HookType.Combat)
+	{
+		public static readonly AddedNode<NEnergyCounter, NGrudgeCounter> GrudgeCounterNode = new(parent =>
+		{
+			var counter = new NGrudgeCounter
+			{
+				Name = "GrudgeCounter",
+				MouseFilter = Control.MouseFilterEnum.Ignore
+			};
 
-            counter.SetAnchorsPreset(Control.LayoutPreset.TopLeft);
-            counter.Position = Vector2.Zero;
-            counter.Size = new Vector2(128, 128);
-            counter.ZIndex = 0;
+			counter.SetAnchorsPreset(Control.LayoutPreset.TopRight);
+			counter.Position = Vector2.Right;
+			counter.Size = new Vector2(200, 200);
+			counter.ZIndex = 0;
 
-            var virtualscene =
-                ResourceLoader.Load<PackedScene>("res://TheMiddleNurseFatherOutis/scenes/GrudgeCounter.tscn");
+			var virtualscene =
+				ResourceLoader.Load<PackedScene>("res://TheMiddleNurseFatherOutis/scenes/GrudgeCounter.tscn");
 
-            var visual = virtualscene.Instantiate<Control>();
-            visual.Name = "Icon";
-            visual.MouseFilter = Control.MouseFilterEnum.Ignore;
+			var visual = virtualscene.Instantiate<Control>();
+			visual.Name = "%Icon";
+			visual.MouseFilter = Control.MouseFilterEnum.Ignore;
 
-            counter.AddChild(visual);
+			counter.AddChild(visual);
 
-            return counter;
+			return counter;
 
-        });
-    }
-    
-    
-    [GlobalClass]
-    public partial class NGrudgeCounter : Control
-    {
-        private Label? _label;
-        private Control? _icon;
-        private NEnergyCounter? _energyCounter;
-        private Player? _player;
+		});
+	}
+	
+	
 
-        private int _grudgeCount = 0;
-
-        public override void _Ready()
-        {
-            this.MouseFilter = MouseFilterEnum.Ignore;
-
-            _label = GetNodeOrNull<Label>("GrudgeCounter/CountContainer/Count");
-            _icon = GetNodeOrNull<Control>("GrudgeCounter/Icon");
-
-            if (GetParent() is NEnergyCounter energyCounter)
-            {
-                _energyCounter = energyCounter;
-            }
-
-            this.Visible = false;
-            
-        }
-
-        public override void _Process(double delta)
-        {
-            if(_player == null) return;
-            RefreshVisiblity();
-            if(this.Visible == false) return;
-            UpdateGrudge();
-        }
-
-        private void RefreshVisiblity()
-        {
-            if (_player == null || _player.PlayerCombatState == null)
-            {
-                this.Visible = false;
-            }
-            else
-            {
-                int Grudge =  GetGrudge(_player);
-
-                this.Visible = this.Visible || _player.Character is Character.TheMiddleNurseFatherOutis || Grudge > 0;
-
-            }
-        }
-
-        private void UpdateGrudge()
-        {
-            if (_player == null || _player.PlayerCombatState == null || _label == null)
-            {
-                return;
-            }
-            int Grudge =  GetGrudge(_player);
-            _grudgeCount = Grudge;
-            _label.Text = _grudgeCount.ToString();
-        }
-    }
 }
---
+++ TheMiddleNurseFatherOutisCode/Grudge/NGrudgeCounter.cs
@@ -0,0 +1,75 @@
+﻿using Godot;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Grudge;
+
+[GlobalClass]
+public partial class NGrudgeCounter : Control
+{
+    private Label? _label;
+    private Control? _icon;
+    private NEnergyCounter? _energyCounter;
+    public Player? _player;
+
+    private int _grudgeCount = 0;
+
+    public override void _Ready()
+    {
+        this.MouseFilter = MouseFilterEnum.Ignore;
+        
+        _label = GetNodeOrNull<Label>("%Count");
+        _icon = GetNodeOrNull<Control>("%Icon");
+
+        if (GetParent() is NEnergyCounter energyCounter)
+        {
+            _energyCounter = energyCounter;
+            _player = energyCounter._player;
+        }
+
+        this.Visible = false;
+        RefreshVisiblity();
+        if (this.Visible == true)
+        {
+            UpdateGrudge();
+        }
+			
+    }
+
+    public override void _Process(double delta)
+    {
+        if(_player == null) return;
+        RefreshVisiblity();
+        if(this.Visible == false) return;
+        UpdateGrudge();
+    }
+
+    private void RefreshVisiblity()
+    {
+        if (_player == null || _player.PlayerCombatState == null)
+        {
+            this.Visible = false;
+        }
+        else
+        {
+            int Grudge =  GrudgeResource.GetGrudge(_player);
+
+            this.Visible = this.Visible || _player.Character is Character.TheMiddleNurseFatherOutis || Grudge > 0;
+            this.Visible = true;
+
+        }
+    }
+
+    private void UpdateGrudge()
+    {
+        if (_player == null || _player.PlayerCombatState == null || _label == null)
+        {
+            return;
+        }
+        MainFile.Logger.Debug($"Updating Grudge");
+        MainFile.Logger.Debug($"Updating Grudge");
+        int Grudge =  GrudgeResource.GetGrudge(_player);
+        _grudgeCount = Grudge;
+        _label.Text = _grudgeCount.ToString();
+    }
+}
