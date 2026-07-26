4d443a81e74ee4b5c4c2859a0e79596c78ca5351
dodo19025 <ydodohdisme@gmail.com>
2026-07-26 18:23:07 +0100

    Finished wild Flailing

---
+++ TheMiddleNurseFatherOutisCode/Cards/Common/Attacks/WildFlailing.cs
@@ -0,0 +1,53 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Character;
+using TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Grudge;
+
+namespace TheMiddleNurseFatherOutis.TheMiddleNurseFatherOutisCode.Cards.Common;
+
+[Pool(typeof(TheMiddleNurseFatherOutisCardPool))]
+
+public class WildFlailing() : TheMiddleNurseFatherOutisCard(
+    1, CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => (
+    [
+        new DamageVar(2m, ValueProp.Move),
+        new DynamicVar("HitAmount", 1m),
+        new DynamicVar("AdditionalHitAmount", 2m),
+        new DynamicVar("GrudgeConsume", 3m),
+    ]);
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        int hitAmount = (int)DynamicVars["HitAmount"].BaseValue;
+        if (GrudgeResource.GetGrudge(Owner.Creature.Player!) >= DynamicVars["GrudgeConsume"].BaseValue)
+        {
+            await GrudgeResource.LoseGrudge((int)DynamicVars["GrudgeConsume"].BaseValue, Owner.Creature.Player!);
+            hitAmount += (int)DynamicVars["AdditionalHitAmount"].BaseValue;
+        }
+        await CommonActions.CardAttack(this, play.Target)
+            .WithHitCount(hitAmount)
+            .Execute(choiceContext);
+    }
+    
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1m);
+
+    }
+}
